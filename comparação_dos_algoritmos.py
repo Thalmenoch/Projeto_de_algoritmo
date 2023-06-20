70dcb150f361c6b04a3326f370f53a00831ce994
Luciano Ramos <thalmenoch@gmail.com>
2023-06-20 16:29:59 -0300

    ajustes e adições
    adição de um alg guloso preliminar e ajuste na main( adição de uma rand para uma entrada de tamanho n com valores aleatórios)

--- comparação_dos_algoritmos.py
+++ comparação_dos_algoritmos.py
@@ -1,13 +1,24 @@
 import time
 from simul import simulation 
+import random
+
+# Além disso, seu c ́odigo deve criar, aleatoriamente e adequadamente, entra-
+# das para o problema. Para cada tamanho de entrada n, vocˆe deve criar m entradas de
+
+# tamanho n e fazer uma m ́edia do tempo de execu ̧c ̃ao para entrada de tamanho n. Vocˆe
+
+# deve comparar os algoritmos de forma justa, ent ̃ao vocˆe deve criar os tamanhos de en-
+# trada e as entradas para executar os algoritmos levando isso em considera ̧c ̃ao.
+
+# criar um rand entre um intervalo considerável pode ser uma boa ideia
 
 tamanhos = [10, 100, 1000]  
 num_execucoes = 5  
 
 for tamanho in tamanhos:
     tempos_execucao = []
-    for _ in range(num_execucoes):
-        entrada = [1] * tamanho  
+    for i in range(num_execucoes):
+        entrada = [random.randint(1, 100) for i in range(tamanho)]  # Cria uma entrada de tamanho n com valores aleatórios
         
         inicio = time.time()
         simulation(entrada)  
